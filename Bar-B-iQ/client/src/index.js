dba2433470c69f3cafb4c98470622ef6f478c6bb
Daniel Hero <danielhero@outlook.com>
2020-07-27 09:48:17 -0500

    fixed api and sql

--- Bar-B-iQ/client/src/index.js
+++ Bar-B-iQ/client/src/index.js
@@ -9,6 +9,7 @@ import * as firebase from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
 };
+console.log(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
